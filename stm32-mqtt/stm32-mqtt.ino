f3114763b011fe0204f72908d124db5bfc0b92fc
Fabrício de Lima Ribeiro <31161759+fabricioitajuba@users.noreply.github.com>
2020-03-18 14:20:09 -0300

    MQTT com ENC28J60

---
+++ stm32-mqtt/stm32-mqtt.ino
@@ -0,0 +1,85 @@
+/*
+ * Programa para teste do protocolo mqtt utilizando o STM32 e o ENC28J60
+ * Autor: Eng. Fabrício de Lima Ribeiro
+ * Data: 18/03/2020
+ * 
+
+ENC28J60  STM32F103
+SCK        PA5
+SO         PA6
+SI         PA7
+CS         PA4
+RST        R
+ 
+VCC - 5V
+GND - GND
+
+ * 
+ * Para testar:
+ * ~$ mosquitto_sub -t "#" -v
+ */
+
+#include <SPI.h>1
+#include <UIPEthernet.h>
+#include "PubSubClient.h"
+
+#define NET_ENC28J60_EIR          0x1C
+#define NET_ENC28J60_ESTAT        0x1D
+#define NET_ENC28J60_ECON1        0x1F
+#define NET_ENC28J60_EIR_RXERIF   0x01
+#define NET_ENC28J60_ESTAT_BUFFER 0x40
+#define NET_ENC28J60_ECON1_RXEN   0x04
+#define NET_ENC28J60_CHECK_PERIOD 5000UL
+
+#define DHCP
+
+#define CLIENT_ID       "UNO"
+#define PUBLISH_DELAY   3000
+
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+
+#ifndef DHCP
+IPAddress   ipdns(192, 168,   1, 1);
+IPAddress      ip(192, 168,   1, 7);//IP fixo
+IPAddress gateway(192, 168,   1, 1);
+IPAddress  subnet(255, 255, 255, 0);
+#endif
+
+EthernetClient ethClient;
+PubSubClient mqttClient;
+
+long previousMillis;
+
+void setup() 
+{
+
+#ifdef  DHCP
+  Ethernet.begin(mac);
+#else
+  Ethernet.begin(mac, ip, ipdns, gateway, subnet);
+#endif
+  
+  // setup mqtt client
+  mqttClient.setClient(ethClient);
+  mqttClient.setServer("192.168.1.100", 1883);
+  previousMillis = millis();
+}
+
+void loop() 
+{
+  if (millis() - previousMillis > PUBLISH_DELAY) 
+  {
+    sendData();
+    previousMillis = millis();
+  }
+
+  mqttClient.loop();
+}
+
+void sendData() 
+{
+  if (mqttClient.connect(CLIENT_ID)) 
+  {
+    mqttClient.publish("/teste/", "Ok!");
+  }
+}
